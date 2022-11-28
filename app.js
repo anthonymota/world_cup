241e451c048bf53457518c3dbb060dad6fc0bd3c
Anthony Mota <anthonymota@MacBook-Pro-2.local>
2022-11-28 12:13:58 -0800

    added a Procfile

--- app.js
+++ app.js
@@ -1604,6 +1604,6 @@ app.get('/', function (req, res) {
   res.send();
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('server running on port 3000');
 });
