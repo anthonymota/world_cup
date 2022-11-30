642290466a68e389f364cb7bdad01c54472539dc
Anthony Mota <anthonymota@MacBook-Pro-2.local>
2022-11-30 11:11:29 -0800

    changed background to fit screen

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ function getResultz(p) {
   reqq.write(dataz);
   reqq.end();
 }
-
+getResultz(38)
 const app = express();
 app.set('view engine', 'ejs');
 app.use('/public', express.static(path.join(__dirname, '/public')));
