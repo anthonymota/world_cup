591137aca3669842bcda58589d02d93b5f35ac01
Anthony Mota <anthonymota@MacBook-Pro-2.local>
2022-12-02 09:25:48 -0800

    ghanavsuruguay koreavsportugal

--- app.js
+++ app.js
@@ -1558,6 +1558,8 @@ app.get('/', function (req, res) {
     ['Morocco', 2, 'Canada', 1],
     ['Costa Rica', 2, 'Germany', 4],
     ['Japan', 2, 'Spain', 1],
+    ['South Korea', 2, 'Portugal', 1],
+    ['Uruguay', 2, 'Ghana', 0],
   ];
   function searchResults(home_team, away_team) {
     for (result of results) {
@@ -1570,7 +1572,7 @@ app.get('/', function (req, res) {
   for (user of picks) {
     console.log('');
     console.log(user.sheet);
-    for (let t = 1; t < 45; t++) {
+    for (let t = 1; t < 47; t++) {
       let element = user.rows[t];
       let user_home_score = element[2];
       let user_away_score = element[4];
